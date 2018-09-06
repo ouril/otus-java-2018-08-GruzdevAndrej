ac176b0c54adaf99f213c474cdbb81cc76f2739c
ouril <ourilkregg@gmail.com>
2018-09-06 14:33:32 +0300

    first commit

---
+++ src/main/java/ru/otus/gruzdev/Main.java
@@ -0,0 +1,10 @@
+package ru.otus.gruzdev;
+
+
+import com.google.common.collect.Lists;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
